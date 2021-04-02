2a9a7e6ace26bc3a603e68df7a067c3184d8f02f
Molly Carroll <mollyajcarroll@gmail.com>
2021-04-02 08:23:48 -0500

    add python-decouple to hide secret key

--- serapis/settings.py
+++ serapis/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,8 +13,7 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u65-tvbsed1ofe^b*muvlvtgh3-hdg02a=^!)h8j(*jq^+h!2g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
