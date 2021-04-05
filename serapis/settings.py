6f68dbd3e26385b95b033dc70819f32679ec7204
Molly Carroll <mollyajcarroll@gmail.com>
2021-04-05 12:11:45 -0500

    update settings for deployment

--- serapis/settings.py
+++ serapis/settings.py
@@ -16,9 +16,9 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mollycarroll.pythonanywhere.com', 'serapisstl.com', 'iloveserapist.com']
 
 
 # Application definition
